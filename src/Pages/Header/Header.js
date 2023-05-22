7f4395bd65d1a2f91496317b46b0d072065c15c8
henytez <119457821+henytez@users.noreply.github.com>
2023-05-22 18:56:07 -0400

    first commit

---
+++ src/Pages/Header/Header.js
@@ -0,0 +1,80 @@
+import React, { useContext } from "react";
+import "./Header.css";
+import Logo from "../../Source/img/evangadi-logo-home.png";
+import { UserContext } from "../../context/UserContext";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  const [userData, setUserData] = useContext(UserContext);
+
+  const logout = () => {
+    //set global state to undefined will logout the user
+    setUserData({
+      token: undefined,
+      user: undefined,
+    });
+    //resetting localStorage
+    localStorage.setItem("auth-token", "");
+  };
+
+  return (
+    <header className="navbar-header">
+      <nav className="navbar navbar-expand-lg fixed-top">
+        <div className="container">
+          <Link className="navbar-brand" to="/">
+            <img src={Logo} alt="Evangadi Logo" />
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarCollapse"
+            aria-controls="navbarCollapse"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon  icon_menu"></span>
+          </button>
+
+          <div
+            className="collapse navbar-collapse bg-orange"
+            id="navbarCollapse"
+          >
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item active">
+                <Link data-scroll="" className="nav-link section-scroll" to="/">
+                  Home
+                </Link>
+              </li>
+              <li className="nav-item">
+                <a
+                  data-scroll=""
+                  className="nav-link section-scroll"
+                  href="/explained"
+                >
+                  How it works
+                </a>
+              </li>
+              <li>
+                <div className="connect-block">
+                  {/* logout when the button clicked in which the function comes from app.js */}
+
+                  <a
+                    className="lnk-toggler btn btn-blue"
+                    data-panel=".panel-login"
+                    href="/login"
+                    onClick={logout}
+                  >
+                    {userData.user ? `Log Out` : `Sign In`}
+                  </a>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/Pages/Question/NewQuestion.js
@@ -0,0 +1,117 @@
+import React, { useState, useContext } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { UserContext } from "../../context/UserContext";
+import Axios from "../../axios";
+import "./question.css";
+const NewQuestion = () => {
+  const [userData] = useContext(UserContext);
+  const axios = Axios();
+  const navigate = useNavigate();
+
+  const [form, setForm] = useState({});
+  //handleChange updates the state of the form object whenever the user types into the input fields
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+  //In the handleSubmit function, the Axios library sends a post request to the server with the user's input data (title and question) as the request body, and the user's token as a config parameter to authenticate the request. If the request is successful, the user is redirected to the homepage. If there's an error, the user is notified with an error message.
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      //sending user data to database to be logged in
+      await axios.post(
+        "/api/question/newquestion",
+        {
+          title: form.title,
+          question: form.question,
+        },
+
+        userData.config
+      );
+      e.target.reset();
+      navigate("/");
+    } catch (err) {
+      console.log("problem", err.response.data.msg);
+      alert(err.response.data.msg);
+    }
+  };
+
+  return (
+    <section className="container" style={{ paddingTop: "100px" }}>
+      <div className="d-flex flex-column align-items-center my-5">
+        <h3>Steps to write a good question</h3>
+        <ul style={{ fontSize: "large", alignItems: "baseline" }}>
+          <li>Summerize your in a one-line title.</li>
+          <li>Describe your problem in more detail.</li>
+          <li>Describe what you tried and what you expected to happen.</li>
+          <li>Review your question and post it to the site.</li>
+        </ul>
+      </div>
+      <div className="container" style={{ width: "90%" }}>
+        <div
+          className="container"
+          style={{
+            paddingTop: "50px",
+            textAlign: "center",
+            fontWeight: "bold",
+          }}
+        >
+          <h3>Ask a public question</h3>
+          <Link to="/">Go to Question Page</Link>
+        </div>
+        <form method="post" onSubmit={handleSubmit}>
+          <div style={{ width: "100%" }}>
+            <input
+              style={{
+                marginTop: "15px",
+                height: "60px",
+                width: "100%",
+                borderRadius: "10px",
+                padding: "10px 15px",
+              }}
+              maxLength="200"
+              type="text"
+              name="title"
+              placeholder="Title"
+              onChange={handleChange}
+            />
+            <div>
+              <textarea
+                style={{
+                  marginTop: "15px",
+                  height: "200px",
+                  width: "100%",
+                  borderRadius: "10px",
+                  padding: "10px 15px",
+                }}
+                maxLength="255"
+                type="text"
+                name="question"
+                placeholder="Question Description..."
+                onChange={handleChange}
+              />
+            </div>
+            <div
+              style={{
+                marginTop: "10px",
+                marginBottom: "10px",
+              }}
+            >
+              <button
+                style={{
+                  padding: "10px 25px",
+                  borderRadius: "5px",
+                }}
+                className="btn btn-lg btn-primary"
+                type="submit"
+              >
+                Post Your Question
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </section>
+  );
+};
+
+export default NewQuestion;
